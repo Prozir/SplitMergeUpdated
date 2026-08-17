5553999e8663e98b5d5c59ae5347421c78291710
Prozir Borah <prozir.borah@outlook.com>
2026-08-17 14:05:35 +0530

    updated node v24

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+process.env.SPFX_OVERRIDE_NODE_VERSION_CHECK = 'true';
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
